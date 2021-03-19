6569e49d367a59bca8b49865e86daef78f809a33
Sander Ledegen <sander.ledegen@outlook.com>
2021-03-19 18:38:51 +0100

    Fourth for loop can be calculated

--- 2015/day-15.js
+++ 2015/day-15.js
@@ -19,59 +19,54 @@ function partOneAndTwo(input) {
         amtThirdIngredient <= 100 - amtFirstIngredient - amtSecondIngredient;
         amtThirdIngredient += 1
       ) {
-        for (
-          let amtFourthIngredient = 0;
-          amtFourthIngredient <=
+        const amtFourthIngredient =
           100 - amtFirstIngredient - amtSecondIngredient - amtThirdIngredient;
-          amtFourthIngredient += 1
-        ) {
-          const totalAmount =
-            amtFirstIngredient + amtSecondIngredient + amtThirdIngredient + amtFourthIngredient;
-          if (totalAmount !== 100) {
-            continue;
-          }
+        const totalAmount =
+          amtFirstIngredient + amtSecondIngredient + amtThirdIngredient + amtFourthIngredient;
+        if (totalAmount !== 100) {
+          continue;
+        }
 
-          const capacity =
-            ingredients[0].capacity * amtFirstIngredient +
-            ingredients[1].capacity * amtSecondIngredient +
-            ingredients[2].capacity * amtThirdIngredient +
-            ingredients[3].capacity * amtFourthIngredient;
+        const capacity =
+          ingredients[0].capacity * amtFirstIngredient +
+          ingredients[1].capacity * amtSecondIngredient +
+          ingredients[2].capacity * amtThirdIngredient +
+          ingredients[3].capacity * amtFourthIngredient;
 
-          const durability =
-            ingredients[0].durability * amtFirstIngredient +
-            ingredients[1].durability * amtSecondIngredient +
-            ingredients[2].durability * amtThirdIngredient +
-            ingredients[3].durability * amtFourthIngredient;
+        const durability =
+          ingredients[0].durability * amtFirstIngredient +
+          ingredients[1].durability * amtSecondIngredient +
+          ingredients[2].durability * amtThirdIngredient +
+          ingredients[3].durability * amtFourthIngredient;
 
-          const flavor =
-            ingredients[0].flavor * amtFirstIngredient +
-            ingredients[1].flavor * amtSecondIngredient +
-            ingredients[2].flavor * amtThirdIngredient +
-            ingredients[3].flavor * amtFourthIngredient;
+        const flavor =
+          ingredients[0].flavor * amtFirstIngredient +
+          ingredients[1].flavor * amtSecondIngredient +
+          ingredients[2].flavor * amtThirdIngredient +
+          ingredients[3].flavor * amtFourthIngredient;
 
-          const texture =
-            ingredients[0].texture * amtFirstIngredient +
-            ingredients[1].texture * amtSecondIngredient +
-            ingredients[2].texture * amtThirdIngredient +
-            ingredients[3].texture * amtFourthIngredient;
+        const texture =
+          ingredients[0].texture * amtFirstIngredient +
+          ingredients[1].texture * amtSecondIngredient +
+          ingredients[2].texture * amtThirdIngredient +
+          ingredients[3].texture * amtFourthIngredient;
 
-          const calories =
-            ingredients[0].calories * amtFirstIngredient +
-            ingredients[1].calories * amtSecondIngredient +
-            ingredients[2].calories * amtThirdIngredient +
-            ingredients[3].calories * amtFourthIngredient;
+        const calories =
+          ingredients[0].calories * amtFirstIngredient +
+          ingredients[1].calories * amtSecondIngredient +
+          ingredients[2].calories * amtThirdIngredient +
+          ingredients[3].calories * amtFourthIngredient;
 
-          const score =
-            Math.max(0, capacity) *
-            Math.max(0, durability) *
-            Math.max(0, flavor) *
-            Math.max(0, texture);
+        const score =
+          Math.max(0, capacity) *
+          Math.max(0, durability) *
+          Math.max(0, flavor) *
+          Math.max(0, texture);
 
-          maxScorePartOne = Math.max(maxScorePartOne, score);
+        maxScorePartOne = Math.max(maxScorePartOne, score);
 
-          if (calories === 500) {
-            maxScorePartTwo = Math.max(maxScorePartTwo, score);
-          }
+        if (calories === 500) {
+          maxScorePartTwo = Math.max(maxScorePartTwo, score);
         }
       }
     }
