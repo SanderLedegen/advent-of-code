1e3857f5d4954b6fc29ac8c876cab2ea16798002
Sander Ledegen <sander@sanderl.be>
2022-12-01 12:01:40 +0100

    AoC 2022 Day 1

---
+++ 2022/day-01.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const input = fs.readFileSync('./inputs/day-01.txt', 'utf-8');
+
+const calsPerElf = (lines) => {
+  return lines.reduce(
+    (acc, line) => {
+      if (line === '') {
+        acc.push(0);
+        return acc;
+      }
+
+      acc[acc.length - 1] += Number(line);
+
+      return acc;
+    },
+    [0]
+  );
+};
+
+function partOne(input) {
+  const lines = input.split('\n');
+  const calories = calsPerElf(lines);
+
+  return Math.max(...calories);
+}
+
+function partTwo(input) {
+  const lines = input.split('\n');
+  const calories = calsPerElf(lines);
+
+  const sorted = calories.sort((a, b) => b - a);
+
+  return sorted[0] + sorted[1] + sorted[2];
+}
+
+console.log(partOne(input));
+console.log(partTwo(input));
